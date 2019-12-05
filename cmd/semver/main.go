a0bcdf5a57b798e592a3229d1ce5e1a38ab813df
Maykon <maykonlfonseca@gmail.com>
2019-12-05 19:08:30 -0300

    Update cmd

--- cmd/semver/main.go
+++ cmd/semver/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "github.com/maykonlf/semver-cli/cmd/semver/cmd"
+import (
+	"fmt"
+	"github.com/maykonlf/semver-cli/internal/commands"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "semver",
+		Short: "Semantic version cli",
+		Long:  "Semantic version tool helper to validate and increase versions semantically",
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(commands.NewValidateCommand().Cmd())
+	rootCmd.AddCommand(commands.NewIncreaseCommand().Cmd())
+	rootCmd.AddCommand(commands.NewSortCommand().Cmd())
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
 
 func main() {
-	cmd.Execute()
+	Execute()
 }
